90e2ac3cb258d30e47a2bf9abdcf928e083020dd
hyungseok Lee <83962223+gudtjr2949@users.noreply.github.com>
2025-01-30 17:26:46 +0900

    Feat: 컴퓨터 난수 생성 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView.printStartGame();
+        do {
+
+        } while (setRetryNumber());
+    }
+
+    private static boolean setRetryNumber() {
+        return true;
     }
 }
---
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -0,0 +1,33 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumber {
+
+    private String computerNumber;
+    private final static int NUMBER_LENGTH = 3;
+
+
+    public ComputerNumber() {
+        this.computerNumber = setComputerNumber();
+    }
+
+
+    /**
+     * 난수 생성
+     * @return 컴퓨터 숫자 (난수)
+     */
+    public String setComputerNumber() {
+        List<String> randomList = new ArrayList<>();
+        for (int i = 0 ; i < NUMBER_LENGTH ; i++) {
+            randomList.add(getRandomNumber());
+        }
+
+        return String.join("", randomList);
+    }
+
+    private static String getRandomNumber() {
+        return Integer.toString((int) ((Math.random() * 9) + 1));
+    }
+}
